891e3f90cde813102785fca9e8f93c843305dc88
theS3b <49106884+theS3b@users.noreply.github.com>
2024-11-18 10:38:23 +0100

    Nice Regression

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,18 +1,23 @@
 import pandas as pd
+from datetime import datetime
 
 def preprocess_data(data, epsilon):
     data.rename(columns={'OBS_VALUE': 'GDP'}, inplace=True)
     data.drop(['Reference area'], axis=1, inplace=True)
     data.dropna(inplace=True)
 
-    data = pd.get_dummies(data, columns=['country'])
+    data['date'] = data['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    data['date'] = (data['date'] - data['date'].min()).dt.days
+    data['date'] = data['date'] / data['date'].max()
 
-    data.set_index('date', inplace=True)
+    data.sort_values('date', inplace=True)
 
-    means = data.mean()
-    stds = data.std()
+    data_encoded = pd.get_dummies(data, columns=['country'])
+    
+    means = data_encoded.mean()
+    stds = data_encoded.std()
 
-    data = (data - means) / (stds + epsilon)
+    data_encoded = (data_encoded - means) / (stds + epsilon)
 
-    return data.drop('GDP', axis=1), data['GDP'], means['GDP'], stds['GDP']
+    return data_encoded.drop('GDP', axis=1), data_encoded['GDP'], data['country'], means['GDP'], stds['GDP']
     
