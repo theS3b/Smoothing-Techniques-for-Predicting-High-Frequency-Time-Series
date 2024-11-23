9630acba93f17a17985bef0ec6750a304a24b8c5
Nathan Gromb <nathan.gromb@epfl.ch>
2024-11-23 12:24:16 +0100

    add lagged gts (no seasonality removal, see other branches)

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-def preprocess_data(data, epsilon, train_pct, mode=None, all_gdps=None, past_gdp_lags=None):
+def preprocess_data(data, epsilon, train_pct, mode=None, all_gdps=None, past_gdp_lags=None, all_gts=None, past_gt_lags=None):
     """
     Preprocess the data for the prediction model
 
@@ -20,6 +20,10 @@ def preprocess_data(data, epsilon, train_pct, mode=None, all_gdps=None, past_gdp
         The GDP values for all the countries, needed to compute the lagged GDP values
     past_gdp_lags : list
         The list of past GDP lags to include in the data
+    all_gts : pd.DataFrame
+        The Google Trends values for all the countries (not preprocessed), needed to compute the lagged Google Trends values
+    past_gt_lags : list
+        The list of past Google Trends lags to include in the data
     """
     if past_gdp_lags:
         if all_gdps is None or any(lag < 1 for lag in past_gdp_lags):
@@ -31,8 +35,18 @@ def preprocess_data(data, epsilon, train_pct, mode=None, all_gdps=None, past_gdp
 
             if mode == 'diff':
                 all_gdps['GDP'] = all_gdps.sort_values('date').groupby('country')['GDP'].diff()
+                print("Warning : Dropping the first row of each country because of the diff operation")
             elif mode == 'pct':
                 all_gdps['GDP'] = all_gdps.sort_values('date').groupby('country')['GDP'].pct_change()
+                print("Warning : Dropping the first row of each country because of the pct operation")
+
+    if past_gt_lags:
+        if all_gts is None or any(lag < 1 for lag in past_gt_lags):
+            raise ValueError("You need to provide all the Google Trends values to include past Google Trends lags, and the lags should be positive integers")
+        else:
+            all_gts = all_gts.copy()
+
+            all_gts['date'] = all_gts['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
 
     data = data.copy()
 
@@ -58,7 +72,21 @@ def preprocess_data(data, epsilon, train_pct, mode=None, all_gdps=None, past_gdp
             data[f'GDP_lag_{lag}'] = data.apply(lambda x: _get_lagged_gdp(x['date'], x['country'], all_gdps=all_gdps, lag=lag), axis=1)
         len_before = len(data)
         data.dropna(inplace=True)
-        print(f"Dropped {len_before - len(data)} rows because of missing lagged GDP values")
+        len_after = len(data)
+        if len_before != len_after:
+            print(f"Dropped {len_before - len_after} rows because of missing lagged GDP values")
+
+    if past_gt_lags:
+        lagged_gts = _get_lagged_gts(all_gts, lags=past_gt_lags)
+
+        data = data.merge(lagged_gts, left_on=["country", "date"], right_on=["country", "date"], how="left")
+
+
+        len_before = len(data)
+        data.dropna(inplace=True)
+        len_after = len(data)
+        if len_before != len_after:
+            print(f"Dropped {len_before - len_after} rows because of missing lagged Google Trends values")
 
     min_date = data['date'].min()
     data['date'] = (data['date'] - min_date).dt.days
@@ -83,6 +111,11 @@ def preprocess_data(data, epsilon, train_pct, mode=None, all_gdps=None, past_gdp
     if past_gdp_lags:
         X_means[X_train.columns.str.contains('GDP_lag')] = y_mean
         X_stds[X_train.columns.str.contains('GDP_lag')] = y_std
+
+    # same for lagged GT values
+    if past_gt_lags:
+        X_means[X_train.columns.str.contains('lag') & ~X_train.columns.str.contains('GDP')] = X_means[X_train.columns.str.contains('average')]
+        X_stds[X_train.columns.str.contains('lag') & ~X_train.columns.str.contains('GDP')] = X_stds[X_train.columns.str.contains('average')]
 
     X_train = _normalize(X_train, X_means, X_stds, epsilon)
     X_valid = _normalize(X_valid, X_means, X_stds, epsilon)
@@ -154,3 +187,20 @@ def _get_lagged_gdp(date, country, all_gdps, lag):
     date_lag = all_dates[curr_date_index[0][0] - lag]
 
     return all_gdps[(all_gdps['date'] == date_lag) & (all_gdps['country'] == country)]['GDP'].values[0]
+
+def _get_lagged_gts(all_gts, lags):
+    """
+    Build the lagged Google Trends values
+    """
+    search_terms = [col for col in all_gts.columns if col.endswith('_average')]
+
+    all_gts[search_terms] = np.log(all_gts[search_terms] + 1)
+
+    for lag in lags:
+        diff = (all_gts[search_terms] - all_gts.groupby("country")[search_terms].diff(3 * lag)).add_suffix(f'_lag_{lag}')
+        all_gts = pd.concat([all_gts, diff], axis=1)
+
+    all_gts.drop(columns=search_terms, inplace=True)
+    all_gts.dropna(inplace=True)
+
+    return all_gts
