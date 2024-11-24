bca90bca155085ee34e63a26b4a991c8a49cae84
Nathan Gromb <nathan.gromb@epfl.ch>
2024-11-24 17:43:18 +0100

    better kf

--- models/KalmanFilterMLP.py
+++ models/KalmanFilterMLP.py
@@ -3,9 +3,10 @@ from filterpy.kalman import KalmanFilter
 from models.MLP import MLP
 
 class KalmanFilterMLP:
-    def __init__(self, seed):
+    def __init__(self, seed, nb_init_samples=5):
         self.seed = seed
         self.nn = MLP(seed=seed)
+        self.nb_init_samples = nb_init_samples
 
         # KFs for each country
         self.kfs = {}
@@ -14,24 +15,28 @@ class KalmanFilterMLP:
         self.nn.fit(X, y)
 
         dt = 1 # Interval between observations
-        k = 2e-5 # Tune : increasing make the model closer to the neural network predictions
+        accel_var = 2e-5 # Acceleration variance, less variance in acceleration means smoother predictions
+                # (because it means that the constant accel model is accurate -> more take into account)
                  # 0 : flat line
                  # 1e-4 : looks good, maybe not smooth enough
                  # 1e-5 : starts lagging
                  # 1 : follow the neural network predictions
 
         nn_predictions = self.nn.predict(X)
-        nn_var = np.var(nn_predictions - y)
+        nn_noise_var = np.var(nn_predictions - y)
 
         self.kfs = {}
         for country in np.unique(countries):
             country_data = nn_predictions[countries == country]
 
             # As initial values, we use the mean of (position, velocity, acceleration) over the first n samples
-            n = 5
-            initial_gdp = np.mean(country_data[:n])
-            initial_velocity = np.mean([country_data[i + 1] - country_data[i] for i in range(n)])
-            initial_acceleration = np.mean([country_data[i + 2] - 2 * country_data[i + 1] + country_data[i] for i in range(n)])
+            first_n_samples = country_data[:self.nb_init_samples]
+            first_n_velocities = [country_data[i + 1] - country_data[i] for i in range(self.nb_init_samples)]
+            first_n_accelerations = [country_data[i + 2] - 2 * country_data[i + 1] + country_data[i] for i in range(self.nb_init_samples)]
+
+            initial_gdp, initial_gdp_var = np.mean(first_n_samples), np.var(first_n_samples)
+            initial_velocity, initial_velocity_var = np.mean(first_n_velocities), np.var(first_n_velocities)
+            initial_acceleration, initial_acceleration_var = np.mean(first_n_accelerations), np.var(first_n_accelerations)
 
             initial_velocity, initial_acceleration = 0, 0
 
@@ -41,12 +46,14 @@ class KalmanFilterMLP:
             self.kfs[country].F = np.array([[1, dt, 0.5 * dt**2],       # position = position + velocity*dt + 0.5*acceleration*dt^2
                                             [0, 1, dt],                 # velocity = velocity + acceleration*dt
                                             [0, 0, 1]])                 # State transition matrix (assuming constant acceleration)
-            self.kfs[country].Q = k * np.eye(3)         # Process noise covariance
+            self.kfs[country].Q = accel_var * np.eye(3)         # Prediction (constant acceleration model) noise covariance
             self.kfs[country].H = np.array([[1, 0, 0]]) # Measurement matrix, only position is observed
-            self.kfs[country].R = nn_var                # Measurement noise covariance, the noise in the neural network predictions
-            self.kfs[country].P = np.eye(3)
-            # TODO : P and Q (process and measurement noise) should have different values for k 
-            # TODO : les KFs sont-ils entrainés séparement
+            self.kfs[country].R = nn_noise_var                # Measurement noise covariance, the noise in the neural network predictions
+            self.kfs[country].P = np.diag([             # State covariance
+                initial_gdp_var,
+                initial_velocity_var,
+                initial_acceleration_var
+            ])
 
     def predict(self, X, countries):
         model_predictions = self.nn.predict(X)
